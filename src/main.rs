63cd8a4ede39d588fd0e084f13773f586c0f65e0
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-02-10 18:31:12 +0200

    Fix indentations

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
 enum Atom {
-  Number(f64),
+    Number(f64),
     Symbol(String)
 }
 
@@ -12,42 +12,42 @@ enum ParseAtomError {
 }
 
 impl fmt::Display for Atom {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    match self {
-      &Atom::Number(n) => {
-        write!(f, "{}", n)
-      },
-      &Atom::Symbol(ref name) => {
-        write!(f, "'{}'", name)
-      }
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Atom::Number(n) => {
+                write!(f, "{}", n)
+            },
+            Atom::Symbol(ref name) => {
+                write!(f, "'{}", name)
+            }
+        }
     }
-  }
 }
 
 impl FromStr for Atom {
-  type Err = ParseAtomError;
+    type Err = ParseAtomError;
 
-  fn from_str(s: &str) -> Result<Atom, ParseAtomError> {
-    match s.parse::<f64>() {
-      Ok(f) => {
-        Ok(Atom::Number(f))
-      },
-      Err(..) => {
-        match s.chars().next() {
-          Some(c) => {
-            if c.is_numeric() {
-              Err(ParseAtomError::IncorrectSymbolName)
-            } else {
-              Ok(Atom::Symbol(s.to_string()))
+    fn from_str(s: &str) -> Result<Atom, ParseAtomError> {
+        match s.parse::<f64>() {
+            Ok(f) => {
+                Ok(Atom::Number(f))
+            },
+            Err(..) => {
+                match s.chars().next() {
+                    Some(c) => {
+                        if c.is_numeric() {
+                            Err(ParseAtomError::IncorrectSymbolName)
+                        } else {
+                            Ok(Atom::Symbol(s.to_string()))
+                        }
+                    },
+                    None => {
+                        Err(ParseAtomError::IncorrectSymbolName)
+                    }
+                }
             }
-          },
-          None => {
-            Err(ParseAtomError::IncorrectSymbolName)
-          }
         }
-      }
     }
-  }
 }
 
 #[cfg_attr(test, allow(dead_code))]
@@ -56,20 +56,20 @@ fn main() {
 
 #[cfg(test)]
 mod test {
-  use super::Atom;
+    use super::Atom;
 
-  #[test]
-  fn parse_integer() {
-    assert_eq!(Atom::Number(64f64), "64".parse::<Atom>().ok().unwrap())
-  }
+    #[test]
+    fn parse_integer() {
+        assert_eq!(Atom::Number(64f64), "64".parse::<Atom>().ok().unwrap())
+    }
 
-  #[test]
-  fn parse_float() {
-    assert_eq!(Atom::Number(64.5), "64.5".parse::<Atom>().ok().unwrap())
-  }
+    #[test]
+    fn parse_float() {
+        assert_eq!(Atom::Number(64.5), "64.5".parse::<Atom>().ok().unwrap())
+    }
 
-  #[test]
-  fn parse_symbol() {
-    assert_eq!(Atom::Symbol("name".to_string()), "name".parse::<Atom>().ok().unwrap())
-  }
+    #[test]
+    fn parse_symbol() {
+        assert_eq!(Atom::Symbol("name".to_string()), "name".parse::<Atom>().ok().unwrap())
+    }
 }
