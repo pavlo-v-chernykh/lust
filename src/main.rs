017ee7e156e5b0a5a2173a7514339b4faeb85b37
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-02-12 14:51:32 +0200

    Implement read function of lisp interpreter

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ enum Atom {
 }
 
 #[derive(Debug, PartialEq)]
+enum Sexp {
+    Atom(Atom),
+    List(Vec<Sexp>)
+}
+
+#[derive(Debug, PartialEq)]
 enum ParseAtomError {
     IncorrectSymbolName
 }
@@ -57,6 +63,87 @@ fn tokenize(s: &str) -> Vec<String> {
         .collect()
 }
 
+#[derive(Debug, PartialEq)]
+enum ReadState {
+    StartRead,
+    OpenList,
+    AtomRead(Atom),
+    CloseList,
+    EndRead,
+}
+
+#[derive(Debug, PartialEq)]
+enum ReadError {
+}
+
+fn read(tokens: Vec<String>) -> Result<Vec<Sexp>, ReadError> {
+    let mut state = ReadState::StartRead;
+    let mut iter = tokens.iter();
+    let mut result = vec![];
+    loop {
+        match state {
+            ReadState::StartRead => {
+                match iter.next() {
+                    Some(s) if *s == "(" => {
+                        state = ReadState::OpenList;
+                        result.push(Sexp::List(vec![]));
+                    },
+                    None => {
+                        state = ReadState::EndRead;
+                    },
+                    _ => {
+                        state = ReadState::EndRead;
+                    }
+                }
+            },
+            ReadState::OpenList => {
+                match iter.next() {
+                    Some(s) if *s == ")" => {
+                        state = ReadState::CloseList;
+                    },
+                    Some(s) => {
+                        state = ReadState::AtomRead(s.parse::<Atom>().ok().unwrap());
+                    },
+                    _ => {
+                        state = ReadState::EndRead;
+                    }
+                }
+            },
+            ReadState::CloseList => {
+                match iter.next() {
+                    Some(s) if *s == "(" => {
+                        state = ReadState::OpenList;
+                        result.push(Sexp::List(vec![]));
+                    },
+                    _ => {
+                        state = ReadState::EndRead;
+                    }
+                }
+            },
+            ReadState::AtomRead(atom) => {
+                if let Some(Sexp::List(mut current_list)) = result.pop() {
+                    current_list.push(Sexp::Atom(atom));
+                    result.push(Sexp::List(current_list));
+                }
+                match iter.next() {
+                    Some(s) if *s == ")" => {
+                        state = ReadState::CloseList;
+                    },
+                    Some(s) => {
+                        state = ReadState::AtomRead(s.parse::<Atom>().ok().unwrap());
+                    },
+                    _ => {
+                        state = ReadState::EndRead;
+                    }
+                }
+            },
+            ReadState::EndRead => {
+                return Ok(result)
+            }
+        }
+    }
+}
+
 #[cfg_attr(test, allow(dead_code))]
 fn main() {
 }
@@ -64,8 +151,9 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::Atom::{self, Number, Symbol};
+    use super::Sexp;
     use super::ParseAtomError::IncorrectSymbolName;
-    use super::tokenize;
+    use super::{tokenize, read};
 
     #[test]
     fn test_parse_integer() {
@@ -103,5 +191,33 @@ mod tests {
                                 .map(|s| { s.to_string() })
                                 .collect();
         assert_eq!(expected_result, tokenize(" ( \n def a\n1)   \n"))
+    }
+
+    #[test]
+    fn test_read_empty() {
+        let expected_result = vec![];
+        assert_eq!(expected_result, read(tokenize("")).ok().unwrap())
+    }
+
+    #[test]
+    fn test_read_single_expression() {
+        let expected_result =  vec![Sexp::List(vec![Sexp::Atom(Symbol("def".to_string())),
+                                                    Sexp::Atom(Symbol("a".to_string())),
+                                                    Sexp::Atom(Number(1f64))])];
+        assert_eq!(expected_result, read(tokenize("(def a 1)")).ok().unwrap())
+    }
+
+    #[test]
+    fn test_read_multiple_expression() {
+        let expected_result =  vec![Sexp::List(vec![Sexp::Atom(Symbol("def".to_string())),
+                                                    Sexp::Atom(Symbol("a".to_string())),
+                                                    Sexp::Atom(Number(1f64))]),
+                                    Sexp::List(vec![Sexp::Atom(Symbol("def".to_string())),
+                                                    Sexp::Atom(Symbol("b".to_string())),
+                                                    Sexp::Atom(Number(2f64))]),
+                                    Sexp::List(vec![Sexp::Atom(Symbol("+".to_string())),
+                                                    Sexp::Atom(Symbol("a".to_string())),
+                                                    Sexp::Atom(Symbol("b".to_string()))])];
+        assert_eq!(expected_result, read(tokenize("(def a 1)(def b 2)(+ a b)")).ok().unwrap())
     }
 }
