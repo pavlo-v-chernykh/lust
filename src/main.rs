c807e12a14f35b4fa00fb748f44dc2d5ac695b1d
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-02-15 22:35:16 +0200

    Add printer module Implement Display trait for Atom and Sexp types

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod common;
 mod lexer;
 mod parser;
 mod interpreter;
+mod printer;
 
 #[cfg_attr(test, allow(dead_code))]
 fn main() {
---
+++ src/printer.rs
@@ -0,0 +1,64 @@
+use std::fmt::{Display, Formatter, Result};
+use common::{Atom, Sexp};
+
+impl Display for Atom {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match *self {
+            Atom::Number(n) => {
+                write!(f, "{}", n)
+            },
+            Atom::Symbol(ref s) => {
+                write!(f, "{}", s)
+            },
+            Atom::Nil => {
+                write!(f, "nil")
+            }
+        }
+    }
+}
+
+impl Display for Vec<Sexp> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "(");
+        let i_last = self.len() - 1;
+        for (i, s) in self.iter().enumerate() {
+            if i < i_last {
+                write!(f, "{} ", s);
+            } else {
+                write!(f, "{}", s);
+            }
+        }
+        write!(f, ")")
+    }
+}
+
+impl Display for Sexp {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match *self {
+            Sexp::Atom(ref atom) => {
+                write!(f, "{}", atom)
+            },
+            Sexp::List(ref l) => {
+                write!(f, "{}", l)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use common::Sexp::{List, Atom};
+    use common::Atom::{Symbol, Number};
+
+    #[test]
+    fn test_format_list_with_nested_list_and_atoms() {
+        let actual_input = List(vec![Atom(Symbol("def".to_string())),
+                                     Atom(Symbol("a".to_string())),
+                                     List(vec![Atom(Symbol("+".to_string())),
+                                               Atom(Number(1f64)),
+                                               Atom(Number(2f64))])]);
+        let actual_result = format!("{}", actual_input);
+        let expected_result = "(def a (+ 1 2))";
+        assert_eq!(expected_result, actual_result);
+    }
+}
