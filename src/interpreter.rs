553915e1af270fbb10dbd9e8896e4990dca62d7d
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-02-16 10:21:20 +0200

    Use recursive call of eval fn for nested list evaluation I'm going to implement frame based evaluation letter.

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -7,24 +7,10 @@ enum EvalError{
     EvalError
 }
 
-struct Frame<'a> {
-    sexp: &'a Sexp,
-    parent: Option<Box<Frame<'a>>>,
-    children: Vec<Frame<'a>>
-}
-
-fn eval(s: Sexp, e: &HashMap<String, Sexp>) -> Result<Sexp, EvalError> {
-    let mut root = Frame {
-        sexp: &s,
-        parent: None,
-        children: vec![]
-    };
-
-    let mut cur_frame = root;
-
-    match *cur_frame.sexp {
+fn eval(s: &Sexp, e: &mut HashMap<String, Sexp>) -> Result<Sexp, EvalError> {
+    match *s {
         Atom(Number(_)) | Atom(Nil) => {
-            Ok(cur_frame.sexp.clone())
+            Ok(s.clone())
         },
         Atom(Symbol(ref name)) => {
             if let Some(s) = e.get(name) {
@@ -35,9 +21,31 @@ fn eval(s: Sexp, e: &HashMap<String, Sexp>) -> Result<Sexp, EvalError> {
         },
         List(ref l) if l.is_empty() => {
             Ok(Atom(Nil))
-        }
-        _ => {
-            Err(EvalError::EvalError)
+        },
+        List(ref l) => {
+            if let Atom(Symbol(ref s)) = *l.first().unwrap() {
+                match &s[] {
+                    "def" => {
+                        if l.len() == 3 {
+                            if let Atom(Symbol(ref n)) = l[2] {
+                                eval(&l[3], e).and_then(|a| {
+                                    e.insert(n.clone(), a.clone());
+                                    Ok(a)
+                                })
+                            } else {
+                                Err(EvalError::EvalError)
+                            }
+                        } else {
+                            Err(EvalError::EvalError)
+                        }
+                    },
+                    _ => {
+                        Err(EvalError::EvalError)
+                    }
+                }
+            } else {
+                Err(EvalError::EvalError)
+            }
         }
     }
 }
@@ -54,7 +62,7 @@ mod tests {
     fn test_eval_atom_number_to_itself() {
         let number = 10f64;
         let expected_result = Atom(Number(number));
-        let actual_result = eval(Atom(Number(number)), &HashMap::new());
+        let actual_result = eval(&Atom(Number(number)), &mut HashMap::new());
         assert_eq!(expected_result, actual_result.ok().unwrap());
     }
 
@@ -64,7 +72,7 @@ mod tests {
         let mut env = HashMap::new();
         env.insert("a".to_string(), Atom(Number(num)));
         let expected_result = Atom(Number(num));
-        let actual_result = eval(Atom(Symbol("a".to_string())), &env);
+        let actual_result = eval(&Atom(Symbol("a".to_string())), &mut env);
         assert_eq!(expected_result, actual_result.ok().unwrap());
     }
 
@@ -73,31 +81,31 @@ mod tests {
         let mut env = HashMap::new();
         env.insert("a".to_string(), Atom(Nil));
         let expected_result = Atom(Nil);
-        let actual_result = eval(Atom(Symbol("a".to_string())), &env);
+        let actual_result = eval(&Atom(Symbol("a".to_string())), &mut env);
         assert_eq!(expected_result, actual_result.ok().unwrap());
     }
 
     #[test]
     fn test_eval_atom_symbol_to_non_value() {
-        let env = HashMap::new();
+        let mut env = HashMap::new();
         let expected_result = EvalError;
-        let actual_result = eval(Atom(Symbol("a".to_string())), &env);
+        let actual_result = eval(&Atom(Symbol("a".to_string())), &mut env);
         assert_eq!(expected_result, actual_result.err().unwrap());
     }
 
     #[test]
     fn test_eval_atom_nil_to_itself() {
-        let env = HashMap::new();
+        let mut env = HashMap::new();
         let expected_result = Atom(Nil);
-        let actual_result = eval(Atom(Nil), &env);
+        let actual_result = eval(&Atom(Nil), &mut env);
         assert_eq!(expected_result, actual_result.ok().unwrap());
     }
 
     #[test]
     fn test_eval_empty_list_to_nil() {
-        let env = HashMap::new();
+        let mut env = HashMap::new();
         let expected_result = Atom(Nil);
-        let actual_result = eval(List(vec![]), &env);
+        let actual_result = eval(&List(vec![]), &mut env);
         assert_eq!(expected_result, actual_result.ok().unwrap());
     }
 }
