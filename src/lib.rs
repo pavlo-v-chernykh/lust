72a3230cf76c7569108a8ffddd4e6269c98512ae
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-04-16 17:05:26 +0300

    Remove namespace and state components

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ mod lexer;
 mod ast;
 mod parser;
 mod scope;
-mod namespace;
-mod state;
 
 pub use parser::Parser;
 pub use scope::Scope;
--- src/state.rs
+++
@@ -1,7 +0,0 @@
-use std::collections::HashMap;
-use namespace::Namespace;
-
-struct State<'ns> {
-    current: String,
-    namespaces: HashMap<String, Namespace<'ns>>
-}
