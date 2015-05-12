841254e28e4d599e22bde8185a27454c0dc49ccb
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-05-12 14:20:41 +0300

    Implement Display trait for Symbol struct

--- src/ast/nodes/symbol.rs
+++ src/ast/nodes/symbol.rs
@@ -1,4 +1,16 @@
+use std::fmt;
+
 pub struct Symbol {
     ns: Option<String>,
     name: String,
+}
+
+impl fmt::Display for Symbol {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let Some(ref ns) = self.ns {
+            write!(f, "{}/{}", ns, self.name)
+        } else {
+            write!(f, "{}", self.name)
+        }
+    }
 }
