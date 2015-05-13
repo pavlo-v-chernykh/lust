3492bf5bf4434a2ebea1198a80ba8c567f4aae1f
Pavel Chernykh <pavel.v.chernykh@gmail.com>
2015-05-13 18:54:58 +0300

    Add constructor and accessor methods to Symbol struct

--- src/ast/nodes/symbol.rs
+++ src/ast/nodes/symbol.rs
@@ -1,8 +1,26 @@
 use std::fmt;
 
+#[derive(Debug, PartialEq, Clone)]
 pub struct Symbol {
     ns: Option<String>,
     name: String,
+}
+
+impl Symbol {
+    fn new(ns: Option<String>, name: String) -> Symbol {
+        Symbol {
+            ns: ns,
+            name: name,
+        }
+    }
+
+    fn name(&self) -> &String {
+        &self.name
+    }
+
+    fn ns(&self) -> Option<&String> {
+        self.ns.as_ref()
+    }
 }
 
 impl fmt::Display for Symbol {
